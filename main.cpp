fc5ba84ce5fee89da463ab2c85a462bcecbc6109
Giuliano <Darkjason55@gmail.com>
2024-07-20 17:42:51 +0300

    lines

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@
 
 #undef main
 
-#define WINDOW_W 800
-#define WINDOW_H 600
+#define RENDER_W 480
+#define RENDER_H 320
 
-#define RENDER_W 10
-#define RENDER_H 10
+#define WINDOW_SCALE 3
+#define WINDOW_W RENDER_W * WINDOW_SCALE
+#define WINDOW_H RENDER_H * WINDOW_SCALE
+
 
 enum class KeyState{KeyDown, KeyUp, KeyRelease, KeyHold};
 KeyState key_list[255];
@@ -102,11 +104,63 @@ void reset_input(){
     }
 }
 
-
 //************** RENDER ******************* 
-void set_pixel(uint32_t* buffer ,vec2 pos, uint32_t color){
-   buffer[int(pos.x + pos.y * RENDER_W)] = color; 
+void draw_pixel(uint32_t* buffer ,vec2 pos, uint32_t color){
+    if(pos.x >= 0 && pos.x < RENDER_W && pos.y >= 0 && pos.y < RENDER_H)
+        buffer[int(pos.x + pos.y * RENDER_W)] = color; 
 }
+
+void draw_line(uint32_t* buffer ,vec2 pos1, vec2 pos2, uint32_t color){
+    bool steep = false;
+    if(abs(pos1.x - pos2.x) < abs(pos1.y - pos2.y)){
+        std::swap(pos1.x, pos1.y);
+        std::swap(pos2.x, pos2.y);
+        steep = true;
+    }
+
+    if(pos1.x > pos2.x){
+        std::swap(pos1.x, pos2.x);
+        std::swap(pos1.y, pos2.y);
+    }
+
+
+    int dx = pos2.x - pos1.x;
+    int dy = pos2.y - pos1.y;
+
+    int derror2 = abs(dy) * 2;
+    int error2 = 0;
+
+    int y = pos1.y;
+
+    for(int x = pos1.x; x <= pos2.x; x++){
+        if(steep){
+            draw_pixel(buffer, vec2{(float)y,(float)x}, color);
+        }else{
+            draw_pixel(buffer, vec2{(float)x,(float)y}, color);
+        }
+        error2 += derror2;
+        if(error2 > dx){
+            y += pos2.y > pos1.y ? 1 : -1;
+            error2 -= dx*2;
+        }
+    }
+}
+
+float a = 0;
+
+
+
+#define MAP_SIZE_X 5
+#define MAP_SIZE_Y 5
+
+int map[25] = {
+    1,1,1,1,1,
+    1,0,0,0,1,
+    1,1,0,1,1,
+    1,0,0,0,1,
+    1,1,1,1,1
+}
+
 
 
 int main(){
@@ -121,7 +175,6 @@ int main(){
     SDL_Texture* frame_buffer = nullptr;
     uint32_t* pixel_buffer;
     int pitch;
-    
 
     if(SDL_Init(SDL_INIT_VIDEO != 0)){
         printf("SDL FAILED TO INITIALIZE");
@@ -145,12 +198,20 @@ int main(){
 
 
     while(window_initialized){
+
+        a += 0.001f;
         pool_events(&event, window_initialized);
 
         SDL_LockTexture(frame_buffer, NULL, (void**)&pixel_buffer, &pitch);
+        memset(pixel_buffer, 0, RENDER_W * RENDER_H);
+    
 
+        //set_pixel(pixel_buffer, vec2{0,0}, 0xff0000);
+        //set_pixel(pixel_buffer, vec2{1,1}, 0xff0000);
 
-        set_pixel(pixel_buffer, vec2{0,0}, 0xff0000);
+        //draw_line(pixel_buffer, vec2{10,10}, vec2{50 + (float)sin(a) * 15,20 + (float)cos(a) * 15}, 0xffffff);
+        //draw_line(pixel_buffer, vec2{20,13}, vec2{40,80}, 0xffff00);
+        //draw_line(pixel_buffer, vec2{80,40}, vec2{13,20}, 0xff0000);
 
 
 
--- src/math.h
+++ src/math.h
@@ -7,3 +7,6 @@ struct vec2i{
     int x;
     int y;
 };
+
+vec2 operator+(vec2 a, vec2 b) { return vec2{a.x + b.x, a.y + b.y};}
+vec2 operator-(vec2 a, vec2 b) { return vec2{a.x - b.x, a.y - b.y};}
